9f1751dae6cd05ebfbd2ffdb8e832754e22058d2
endy <7ofpentacles@gmail.com>
2022-02-16 21:28:02 +0700

    add user mutation

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './user.module';
+import { AppModule } from './app.module';
 import { Transport, TcpOptions } from '@nestjs/microservices';
 
 import { ConfigService } from './services/config/config.service';
 
-async function bootstrap() {
-  console.log('GATEWAY PORT ', process.env.USER_SERVICE_PORT);
+// async function bootstrap() {
+//   console.log('USER PORT ', process.env.USER_SERVICE_PORT);
 
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.TCP,
-    options: {
-      host: '0.0.0.0',
-      port: new ConfigService().get('port'),
-    },
-  } as TcpOptions);
-  await app.listen();
+//   const app = await NestFactory.createMicroservice(AppModule, {
+//     transport: Transport.TCP,
+//     options: {
+//       host: '0.0.0.0',
+//       port: new ConfigService().get('port'),
+//     },
+//   } as TcpOptions);
+
+//   await app.listen();
+// }
+// bootstrap();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  console.log('USER PORT ', new ConfigService().get('port'));
+
+  await app.listen(new ConfigService().get('port'));
 }
 bootstrap();
